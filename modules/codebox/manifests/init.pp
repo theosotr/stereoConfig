25d1a0061e9c57732ab77350a9b15baaa4736456
Marios Fragkoulis <mfg@aueb.gr>
2015-11-09 19:16:55 +0200

    Added manifests, modules, bootstrap script, and node classification manifest.

---
+++ modules/codebox/manifests/init.pp
@@ -0,0 +1,6 @@
+class codebox {
+  include apt
+  include nodejs
+  include build_essential
+  include codebox::cb
+}
---
+++ modules/convenience/manifests/init.pp
@@ -0,0 +1,32 @@
+# Various convenience packages
+class convenience {
+
+  package{'sudo': }
+  package{'bzip2': }
+  package{'bzr': }
+  package{'curl': }
+  package{'dnsutils': }         # Dig and friends
+  package{'finger': }
+  package{'ftp': }
+  package{'g++': }
+  package{'gdb': }
+  package{'git': }
+  package{'graphviz': }
+  package{'locate': }
+  package{'mercurial': }
+  package{'strace': }
+  package{'telnet': }
+  package{'unzip': }
+  package{'zip': }
+  package{'python-pip': }
+  package{'emacs': }
+  package{'default-jdk': }
+  package{'ant': }
+  package{'maven': }
+  package{'mysql-server': }
+  package{'mongodb': }
+
+  package{'vim-puppet': }
+
+}
+
