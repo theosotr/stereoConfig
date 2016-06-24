13392fa052ef5e698247bf8b7833e811770b78ee
Tushar <tushar@aueb.gr>
2016-06-24 23:12:33 +0300

    added grub setting module

---
+++ modules/grub/manifests/init.pp
@@ -0,0 +1,8 @@
+file { '/etc/default/grub':
+  ensure => present,
+}->
+  file_line { 'grubSetting':
+    path => '/etc/default/grub',
+    line => 'GRUB_CMDLINE_LINUX_DEFAULT="intel_pstate=disable quiet splash"',
+    match => '^GRUB_CMDLINE_LINUX_DEFAULT=.*$',
+  }
