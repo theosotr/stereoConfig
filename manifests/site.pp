62a324b0c14a18a8610167865184400eeaf811d8
Tushar <tushar@aueb.gr>
2016-06-26 14:43:35 +0300

    minor changes to make changes in grub settings

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,5 @@ node default {
 #  include ::convenience
   include ::allpackages
   include ::services
+  include ::grub
 }
--- modules/grub/manifests/init.pp
+++ modules/grub/manifests/init.pp
@@ -1,8 +1,10 @@
-file { '/etc/default/grub':
-  ensure => present,
-}->
-  file_line { 'grubSetting':
-    path => '/etc/default/grub',
-    line => 'GRUB_CMDLINE_LINUX_DEFAULT="intel_pstate=disable quiet splash"',
-    match => '^GRUB_CMDLINE_LINUX_DEFAULT=.*$',
-  }
+class grub {
+  file { '/etc/default/grub':
+    ensure => present,
+  }->
+    file_line { 'grubSetting':
+      path => '/etc/default/grub',
+      line => 'GRUB_CMDLINE_LINUX_DEFAULT="intel_pstate=disable quiet splash"',
+      match => '^GRUB_CMDLINE_LINUX_DEFAULT=.*$',
+    }
+}
