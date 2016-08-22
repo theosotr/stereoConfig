104b53903307a4d6d95761adf39a80e4dd0f4517
Tushar <tushar@aueb.gr>
2016-08-22 13:36:55 +0300

    added new apt source

--- modules/apt-pkg/manifests/init.pp
+++ modules/apt-pkg/manifests/init.pp
@@ -6,4 +6,11 @@ class apt-pkg {
   package{'aptdaemon-data': }
   package{'aptitude': }
   package{'aptitude-common': }
+  
+  File [ '/etc/apt/sources.list']
+  ->
+    file_line { 'add-sources':
+      path => '/etc/apt/sources.list',
+      line => 'deb http://cran.uni-muenster.de/bin/linux/debian jessie-cran3/',
+    }
 }
