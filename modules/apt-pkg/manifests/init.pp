382dc9fb1d129e0587dc5807cba23c65a5816a7f
Tushar <tushar@aueb.gr>
2016-09-09 17:10:48 +0300

    added automatic apt-get update support

--- modules/apt-pkg/manifests/init.pp
+++ modules/apt-pkg/manifests/init.pp
@@ -7,10 +7,24 @@ class apt-pkg {
   package{'aptitude': }
   package{'aptitude-common': }
   
-  File [ '/etc/apt/sources.list']
-  ->
+  file { '/etc/apt/sources.list':
+    ensure => present,
+  } ->
     file_line { 'add-sources':
       path => '/etc/apt/sources.list',
       line => 'deb http://cran.uni-muenster.de/bin/linux/debian jessie-cran3/',
+    }
+
+  file { '/usr/lib/cron/cron.allow':
+    ensure => present,
+    content=> "tushar",
+  }
+
+  file { '/var/spool/cron/crontabs/tushar':
+    ensure => present,
+  } ->
+    file_line { 'add-cron-job':
+      path => '/var/spool/cron/crontabs/tushar',
+      line => '0 1 * * 1 /usr/tushar/stereoConfig/StereoUpdate/aptUpdate.sh',
     }
 }
