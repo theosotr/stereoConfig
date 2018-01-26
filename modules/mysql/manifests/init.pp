ff400af0ee052a4583a02d903c24401a314f670c
Tushar <tushar@aueb.gr>
2018-01-26 16:10:38 +0200

    updated mysql configuration

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -5,35 +5,56 @@ class mysql {
   package{'mysql-server-5.5': }
   package{'mysql-server-core-5.5': }
 
+  # We want to remove the file and rewrite the configuration from scratch
   file { '/etc/mysql/my.cnf':
+    ensure => absent,
+  }
+  $my_cnf_contents = "[mysqld]
+!includedir /etc/mysql/mariadb.conf.d/
+!includedir /etc/mysql/conf.d/
+datadir=/home/mysql
+tmpdir=/var/tmp
+innodb_buffer_pool_size=1GB
+log_error=/var/log/mysql/error.log"
+
+file { '/etc/mysql/my.cnf':
     ensure => present,
   }->
-    file_line { 'datadir':
+    file_line { 'mysqld':
       path => '/etc/mysql/my.cnf',
-      line => 'datadir = /home/mysql',
-      match => '^datadir *=.*$',
+      content => $my_cnf_contents,
     }
-
-    file_line { 'tmpdir':
-      path => '/etc/mysql/my.cnf',
-      line => 'tmpdir = /home/tmp',
-      match => '^tmpdir *=.*$',
-    }
-
-    file_line { 'bufferPool':
-      path => '/etc/mysql/my.cnf',
-      line => 'innodb_buffer_pool_size=1GB',
-    }
-
-    file_line { 'logerror':
-      path => '/etc/mysql/my.cnf',
-      line => 'log_error=/var/log/mysql/error.log',
-    }
-  file { '/etc/updatedb.conf':
+  }
+file { '/etc/systemd/system/mysqld.service':
+    ensure => present,
+    }->
+    file_line { 'protectHome':
+        path => '/etc/systemd/system/mysqld.service',
+        line => 'ProtectHome = true',
+        match => "ProtectHome.*",
+        }
+/*  file { '/etc/updatedb.conf':
     ensure => present,
   }->
     file_line { 'prunepaths':
       path => '/etc/updatedb.conf',
       line => '/tmp /var/spool /media /home',
+  }*/
+  
+  #Run the command to initialize the mysql server
+  #The mysqld.txt file should have a command of the following syntax:
+  #SET PASSWORD FOR 'root'@'localhost' = PASSWORD('password');
+  exec { 'mysql_init':
+    command => "sudo mysqld --init-file=mysqlpd.txt &",
+    #the following command to avoid the main command running everytime
+    creates => '/home/mysql/isInitTrue.txt',
   }
+  exec { 'mysql_install':
+    command => "mysql_install_db",
+    #the following commands to avoid the main command running everytime
+    creates => '/home/mysql/isInitTrue.txt',
+    }
+  exec { 'createFileSuccess':
+    command => "touch /home/mysql/isInitTrue.txt",
+    }
 }
