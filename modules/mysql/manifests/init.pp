a47d959db6ec7484e142e303770832a47dd96b4a
Thodoris Sotiropoulos <theosotr@aueb.gr>
2019-01-20 21:35:18 +0200

    Set ordering relationships in mysql module

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -16,42 +16,65 @@ innodb_log_file_size=256M
 key_buffer_size=5GB
 log_error=/var/log/mysql/error.log"
 
-file { '/home/mysql/tmp':
-    ensure => directory,
-    mode  => '1777'
-}
+  file { '/home/mysql/tmp':
+      ensure => directory,
+      mode  => '1777'
+  }
 
-file { '/etc/mysql/my.cnf':
-    ensure => present,
-#  }->
-#    file_line { 'mysqld':
-#      path => '/etc/mysql/my.cnf',
+  file { '/etc/mysql/my.cnf':
+      ensure  => present,
+      require => [
+        Package['mysql-common'],
+        Package['mysql-server']
+      ],
       content => $my_cnf_contents,
-    }
-file { '/etc/systemd/system/mysqld.service':
-    ensure => present,
-    }->
-    file_line { 'protectHome':
-        path => '/etc/systemd/system/mysqld.service',
-        line => 'ProtectHome = true',
-        match => "ProtectHome.*",
-        }
+  }
+
+  file { '/etc/systemd/system/mysqld.service':
+      ensure  => present,
+      require => [
+        Package['mysql-common'],
+        Package['mysql-server']
+      ],
+  }
+
+  file_line { 'protectHome':
+      path    => '/etc/systemd/system/mysqld.service',
+      line    => 'ProtectHome = true',
+      match   => "ProtectHome.*",
+      require => [
+        Package['mysql-common'],
+        Package['mysql-server'],
+        File['/etc/systemd/system/mysqld.service']
+      ],
+  }
 /*  file { '/etc/updatedb.conf':
     ensure => present,
   }->
-    file_line { 'prunepaths':
+  file_line { 'prunepaths':
       path => '/etc/updatedb.conf',
       line => '/tmp /var/spool /media /home',
   }*/
   
-file { '/lib/systemd/system/mariadb.service':
-    ensure => present,
-    }->
-    file_line { 'protectHomeFalse':
-        path => '/lib/systemd/system/mariadb.service',
-        line => 'ProtectHome = false',
-        match => "ProtectHome.*",
-        }
+  file { '/lib/systemd/system/mariadb.service':
+      ensure  => present,
+      require => [
+        Package['mysql-common'],
+        Package['mysql-server'],
+        File['/etc/systemd/system/mysqld.service']
+      ],
+  }
+
+  file_line { 'protectHomeFalse':
+      path    => '/lib/systemd/system/mariadb.service',
+      line    => 'ProtectHome = false',
+      match   => "ProtectHome.*",
+      require => [
+        Package['mysql-common'],
+        Package['mysql-server'],
+        File['/lib/systemd/system/mysqld.service']
+      ],
+  }
         
   #Run the command to initialize the mysql server
   #The mysqld.txt file should have a command of the following syntax:
@@ -62,14 +85,25 @@ file { '/lib/systemd/system/mariadb.service':
     #the following command to avoid the main command running everytime
     creates => '/home/mysql/isInitTrue.txt',
   }
+
   exec { 'mysql_install':
     command => "mysql_install_db",
-    path => '/bin,/sbin,...',
+    path    => '/bin,/sbin,...',
     #the following commands to avoid the main command running everytime
     creates => '/home/mysql/isInitTrue.txt',
-    }
+    require =>  [
+      Package['mysql-common'],
+      Package['mysql-server'],
+      File['/etc/mysql/my.cnf']
+    ]
+  }
+
   exec { 'createFileSuccess':
     command => "touch /home/mysql/isInitTrue.txt",
-    path => '/bin,/sbin,...',  
+    path    => '/bin,/sbin,...',
+    require =>  [
+      Exec['mysql_init'],
+      Exec['mysql_install']
+    ]
   }
 }
